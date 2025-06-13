0bcec2944ff410e584dd1d75624262aca78a2f27
Leandro Antony Batista Lemos <20221074010005@ifrn.local>
2025-06-13 14:04:46 -0300

    Commit 13/06/2025

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header({children}) {
     return (
         <div className={styles.header}>
             <img className={styles.img} src={tomato} alt="Tomato" />
-            <h1>{children}</h1>
+            <h1 className={styles.phrase}>{children}</h1>
         </div>
     )
     }

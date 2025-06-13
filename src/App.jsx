2fe5b0196d058758a2ed5854d4fd6258595fb735
NEGRESC0 <156157000+NEGRESC0@users.noreply.github.com>
2025-06-13 01:13:42 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header'
+import CardList from './components/CardList'
+import Footer from './components/Footer'
+
+export default function App() {
+  return (
+    <>
+      <Header>Foco, Força e Fé</Header>
+      <CardList />
+      <Footer txt="IFRN - Campus Macau
+       Curso Técnico em Informática
+       Programação para Internet 2025" nome="Leandro Antony Batista Lemos" github="https://github.com/Leandro-Antony" instagram="https://www.instagram.com/leandro_013__/" whatsapp="" ></Footer>
+    </>
+  )
+}
